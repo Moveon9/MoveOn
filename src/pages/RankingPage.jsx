f7a557740af6e192b25d0bdd5ed5f8e22327c8dd
Lseojeong <sjung0314@gachon.ac.kr>
2025-05-02 12:04:01 +0900

    [feat] ADD nav bar

---
+++ src/pages/RankingPage.jsx
@@ -0,0 +1,12 @@
+// src/pages/MainPage.jsx
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function RankingPage() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ranking Page</Text>
+    </View>
+  );
+}
